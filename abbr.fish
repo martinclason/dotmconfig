856c221a65afd246a838cac99d96e2a46be23368
Martin Clason <martin@clason.se>
2019-06-16 17:04:53 +0200

    Updated and added config for fish

---
+++ abbr.fish
@@ -0,0 +1,15 @@
+if status --is-interactive
+    set -g fish_user_abbreviations
+
+    abbr g git
+    abbr gc 'git commit'
+    abbr gcm 'git commit -m'
+    abbr ga 'git add'
+    abbr gaa 'git add --all'
+    abbr gp 'git pull'
+    abbr gs 'git status'
+    abbr gco 'git checkout'
+
+    abbr l "less"
+end
+
--- config.fish
+++ config.fish
@@ -1,17 +1,31 @@
+set fish_user_paths /usr/local/bin /usr/bin /bin /usr/sbin /sbin /opt/local/bin /opt/local/sbin /Users/martinclason/narwhal/bin /Users/martinclason/opt/GNAT/2018/bin /opt/X11/bin /usr/local/share/dotnet /Users/martinclason/Utveckling/Terminal/bin /Users/martinclason/.npm-packages/bin /Users/martinclason/anaconda3/bin /Users/martinclason/.local/bin
+
 set -gx NARWHAL_ENGINE jsc
 set -gx CAPP_BUILD "/Users/martinclason/Utveckling/Cappuccino/Build"
 #set -gx EDITOR "Visual Studio Code"
 set -gx LANG "en_GB.UTF-8"
-alias spacemacs "env HOME=$HOME/spacemacs emacs"
 
-alias gs "git status"
-alias gc "git commit"
-alias gcm "git commit -m"
-alias gcam "git commit -am"
-alias gaa "git add -A"
 
-alias l "less"
+#alias spacemacs "env HOME=$HOME/spacemacs emacs"
+#alias gs "git status"
+#alias gc "git commit"
+#alias gcm "git commit -m"
+#alias gcam "git commit -am"
+#alias gaa "git add -A"
 
-alias config='/usr/bin/git --git-dir=$HOME/.mconfig.git/ --work-tree=$HOME'
+#alias l "less"
+
+#alias config='/usr/bin/git --git-dir=$HOME/.mconfig.git/ --work-tree=$HOME'
 #alias emacmacs='/Applications/Emacmacs.app/Contents/MacOS/Emacs'
 #alias unidrive="cd '/Users/martinclason/Library/Mobile Documents/3L68KQB4HG~com~readdle~CommonDocuments/Documents'"
+
+set -x EDITOR vim
+
+# Abbreviations
+if test -f ./abbr.fish
+    source abbr.fish
+end
+
+function mkcd 
+    mkdir $argv && cd $argv
+end
