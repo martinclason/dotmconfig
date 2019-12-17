ad137dde185526073069abcaef0eaf37d1453001
Martin Clason <martin@clason.se>
2019-12-17 22:49:19 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -2,39 +2,33 @@
 
 # Tries to create $2 linking to $1
 try_link() {
-    if [ ! -e $2 ]; then
+    if [ ! -e "$2" ]; then
         ln -s $PWD/$1 $2
-        echo "Created link $2"
+        echo "CREATED LINK: $2"
     else
-        echo "$2 already exists, please fix this manually if needed."
+        echo "NOTHING DONE: $2 already exists, please fix this manually if needed."
     fi
 }
 
+echo "Installing mconfig config-files.\n"
+
 # Editors
-## Install vim dir
-if [ ! -d "$HOME/.vim" ]; then
-    echo "Creating .vim directory"
-    mkdir -p ~/.vim/autoload
-    cp .vim/autoload/plug.vim ~/.vim/autoload/
-fi
-
 try_link .vimrc ~/.vimrc
-
-## Install .emacs.d dir
-if [ ! -d "$HOME/.emacs.d" ]; then
-    echo "Creating .emacs.d directory"
-    mkdir ~/.emacs.d
-fi
-
+mkdir -p ~/.vim/autoload
+try_link .vim/autoload/plug.vim ~/.vim/autoload/plug.vim
+mkdir -p ~/.config/nvim
+try_link init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.emacs.d
 try_link .emacs.d/init.el ~/.emacs.d/init.el
 
-try_link init.vim ~/.config/nvim/init.vim
 
 # Fish
+mkdir -p ~/.config/fish
 try_link config.fish ~/.config/fish/config.fish
 try_link abbr.fish ~/.config/fish/abbr.fish
 
 # Misc
+mkdir -p ~/.config/alacritty
 try_link alacritty.yml ~/.config/alacritty/alacritty.yml
 try_link .tmux.conf ~/.tmux.conf
 try_link .inputrc ~/.inputrc
