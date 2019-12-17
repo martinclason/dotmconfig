26e01225758d7029ebc551b033ffc9a4d1d0f143
Martin Clason <martin@clason.se>
2019-12-17 22:23:59 +0100

    Created shell script for install

--- install.sh
+++ install.sh
@@ -1,28 +1,44 @@
-#!/usr/local/bin/fish
+#!/bin/sh
 
-function try_link
-  if ! test -f $argv[2]
-    ln -s $PWD/$argv[1] $argv[2]
-    echo "link $argv[2] created for $argv[1]"
-  else
-    echo "$argv[2] already exists. Please fix this manually."
-  end
-end
+# Tries to create $2 linking to $1
+try_link() {
+    if [ ! -e $2 ]; then
+        ln -s $PWD/$1 $2
+        echo "Created link $2"
+    else
+        echo "$2 already exists, please fix this manually if needed."
+    fi
+}
 
+# Editors
+## Install vim dir
+if [ ! -d "$HOME/.vim" ]; then
+    echo "Creating .vim directory"
+    mkdir -p ~/.vim/autoload
+    cp .vim/autoload/plug.vim ~/.vim/autoload/
+fi
 
-echo "Installing mconfig dotfiles..."
+try_link .vimrc ~/.vimrc
+
+## Install .emacs.d dir
+if [ ! -d "$HOME/.emacs.d" ]; then
+    echo "Creating .emacs.d directory"
+    mkdir ~/.emacs.d
+fi
 
 try_link .emacs.d/init.el ~/.emacs.d/init.el
+
+try_link init.vim ~/.config/nvim/init.vim
+
+# Fish
 try_link config.fish ~/.config/fish/config.fish
 try_link abbr.fish ~/.config/fish/abbr.fish
-#try_link .vimrc ~/.vimrc
-try_link .vim ~/.vim
-try_link init.vim ~/.config/nvim/init.vim
+
+# Misc
 try_link alacritty.yml ~/.config/alacritty/alacritty.yml
 try_link .tmux.conf ~/.tmux.conf
 try_link .inputrc ~/.inputrc
-#ln -s ~/.emacs.d/init.el ./.emacs.d/init.el
-#ln -s ~/.config/fish/config.fish ./config.fish
-#ln -s ~/.vimrc ./.vimrc
+
 # Preferences
 try_link Preferences/net.ankiweb.dtop.plist ~/Library/Preferences/net.ankiweb.dtop.plist 
+
