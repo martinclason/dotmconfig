2d459ac740781f1e7e7c9b4a414ed325de3d0c22
Martin Clason <martin@clason.se>
2022-12-18 00:23:43 +0100

    fish fixes to e.g. path

--- config.fish
+++ config.fish
@@ -1,13 +1,13 @@
-#set fish_user_paths /usr/local/bin /usr/bin /bin /usr/sbin /sbin /opt/local/bin /opt/local/sbin /Users/martinclason/narwhal/bin /Users/martinclason/opt/GNAT/2018/bin /opt/X11/bin /usr/local/share/dotnet /Users/martinclason/Utveckling/Terminal/bin /Users/martinclason/.npm-packages/bin /Users/martinclason/anaconda3/bin /Users/martinclason/.local/bin ~/.cabal/bin /Users/martinclason/.ghcup/bin
+#set fish_user_paths /usr/local/bin /usr/bin /bin /usr/sbin /sbin /opt/local/bin /opt/local/sbin /Users/martinclason/narwhal/bin /Users/martinclason/opt/GNAT/2019/bin /opt/X11/bin /usr/local/share/dotnet /Users/martinclason/Utveckling/Terminal/bin /Users/martinclason/.npm-packages/bin /Users/martinclason/anaconda3/bin /Users/martinclason/.local/bin ~/.cabal/bin /Users/martinclason/.ghcup/bin
 # Changed path to this to fix python error when starting swift REPL
-set fish_user_paths /usr/local/bin /usr/bin /usr/bin /bin /usr/sbin /sbin /opt/local/bin /opt/local/sbin /usr/local/sbin /Users/martinclason/narwhal/bin /Users/martinclason/opt/GNAT/2018/bin /opt/X11/bin /usr/local/share/dotnet /Users/martinclason/Utveckling/Terminal/bin /Users/martinclason/.npm-packages/bin /Users/martinclason/anaconda3/bin /Users/martinclason/.local/bin ~/.cabal/bin /Users/martinclason/.ghcup/bin /usr/bin/swift /Users/martinclason/opt/GNAT/2019/bin ~/bin /Users/martinclason/.poetry/bin
-
+set fish_user_paths /usr/local/bin /usr/bin /usr/bin /bin /usr/sbin /sbin /opt/local/bin /opt/local/sbin /usr/local/sbin /Users/martinclason/narwhal/bin /Users/martinclason/opt/GNAT/2018/bin /opt/X11/bin /usr/local/share/dotnet /Users/martinclason/Utveckling/Terminal/bin /Users/martinclason/.npm-packages/bin /Users/martinclason/anaconda3/bin /Users/martinclason/.local/bin /usr/local/Cellar/llvm/12.0.0/bin ~/.cabal/bin /Users/martinclason/.ghcup/bin /usr/bin/swift /Users/martinclason/opt/GNAT/2019/bin ~/bin /Users/martinclason/.poetry/bin 
 set -gx NARWHAL_ENGINE jsc
 set -gx CAPP_BUILD "/Users/martinclason/Utveckling/Cappuccino/Build"
 #set -gx EDITOR "Visual Studio Code"
 set -gx LANG "en_GB.UTF-8"
 set -g Z_SCRIPT_PATH ~/.z
 set -g theme_date_format "+%H:%M"
+set -g IHP_EDITOR "sublime"
 #set -g TERM "xterm+256color"
 #set -g TERM "xterm-256color-italic"
 
@@ -26,6 +26,7 @@ set -g theme_date_format "+%H:%M"
 
 alias tvim "env TERM=xterm-256color vim"
 alias clang++17 "clang++ -std=c++17 -Wall -Wextra -pedantic -Weffc++ -Wold-style-cast -Woverloaded-virtual -fmax-errors=3 -g"
+alias c++20 "clang++ -std=c++20 -Wall -Wextra -pedantic -Weffc++ -Wold-style-cast -Woverloaded-virtual -ferror-limit=3 -g"
 
 set -gx GETLAB_LOCATION "~/unidrive/UPP/getlab-cli"
 
@@ -33,6 +34,9 @@ alias getlab "$GETLAB_LOCATION/staff_cli.py"
 alias glabs "cd $GETLAB_LOCATION/labs"
 
 set -x EDITOR vim
+
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+set -gx STARSHIP_CONFIG "~/.config/starship.toml"
 
 # Abbreviations
 if test -f ~/.config/fish/abbr.fish
@@ -49,3 +53,19 @@ eval /Users/martinclason/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 #bass source ~/.nvm/nvm.sh --no-use ';' nvm use node
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; test -f /Users/martinclason/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /Users/martinclason/.ghcup/bin $PATH # ghcup-env
+
+# This should be manually run to create aliases for node, yarn... see https://github.com/FabioAntunes/fish-nvm
+# nvm_alias_command
+
+# starship init fish | source
+
+# Nix försökte ursprungligen skriva detta till /usr/local/etc/fish/conf.d/nix.fish
+# # Nix
+# if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
+#   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
+# end
+# # End Nix
+
+# https://direnv.net/docs/hook.html
+direnv hook fish | source
